f48500dfa9a104bf8784837604fa8d6dd4a2ab71
britte <lizzy.britton@gmail.com>
2015-02-22 17:21:06 -0500

    Add servlet mapping to xml parser

--- HW1/src/edu/upenn/cis/cis455/webserver/XmlParser.java
+++ HW1/src/edu/upenn/cis/cis455/webserver/XmlParser.java
@@ -12,10 +12,11 @@ import org.xml.sax.helpers.DefaultHandler;
 
 public class XmlParser {
 	
-	private Logger logger = Logger.getLogger(HttpServer.class);	
+	private static Logger logger = Logger.getLogger(HttpServer.class);	
 	
 	private String path;
 	private HashMap<String,String> servlets;
+	private HashMap<String,String> servletMappings;
 	private HashMap<String,String> contextParams;
 	private HashMap<String, HashMap<String,String>> servletParams;
 	
@@ -28,17 +29,19 @@ public class XmlParser {
 	 * Handler class for parsing out servlet xml files.Passed into a SAXParser.
 	 */
 	static class Handler extends DefaultHandler {
-		private int m_state = 0; // state representing servlet element
+		private int m_state = 0;
 		private String m_servletName;
 		private String m_paramName;
+		private String m_mappingName;
 		HashMap<String,String> m_servlets = new HashMap<String,String>();
+		HashMap<String,String> m_servletMappings = new HashMap<String,String>();
 		HashMap<String,String> m_contextParams = new HashMap<String,String>();
 		HashMap<String,HashMap<String,String>> m_servletParams = new HashMap<String,HashMap<String,String>>();
-//		HashMap<String,String> m_servletMappings = new HashMap
 		
 		public void startElement(String uri, String localName, String qName, Attributes attributes) {
+			logger.info(qName);
 			if (qName.compareTo("servlet-name") == 0) {
-				m_state = 1;
+				m_state = (m_state == 0) ? 1 : 50;
 			} else if (qName.compareTo("servlet-class") == 0) {
 				m_state = 2;
 			} else if (qName.compareTo("context-param") == 0) {
@@ -46,26 +49,28 @@ public class XmlParser {
 			} else if (qName.compareTo("init-param") == 0) {
 				m_state = 4;
 			} else if (qName.compareTo("param-name") == 0) {
-				// check if you are within a context-param element
-				// or a servlet's init-param element
 				m_state = (m_state == 3) ? 30 : 40;
 			} else if (qName.compareTo("param-value") == 0) {
-				// check if you are within a context-param element
-				// or a servlet's init-param element
 				m_state = (m_state == 30) ? 31 : 41;
+			} else if (qName.compareTo("servlet-mapping") == 0) {
+				m_state = 5;
+			} else if (qName.compareTo("url-pattern") == 0) {
+				m_state = 51;
 			}
 		}
 		public void characters(char[] ch, int start, int length) {
 			String value = new String(ch, start, length);
-			if (m_state == 1) { // servlet-name
+			if (m_state == 1) {
 				m_servletName = value;
 				m_state = 0;
-			} else if (m_state == 2) { // servlet-class
+			} else if (m_state == 2) {
 				m_servlets.put(m_servletName, value);
-				m_state = 0; 
-			} else if (m_state == 30 || m_state == 40) { // init-param || context-param > param-name
-				m_paramName = value;
-			} else if (m_state == 31) { // context-param > param-value
+				m_state = 0;
+			} else if (m_state == 30 || m_state == 40) {
+				if (value.trim().length() > 0) {
+					m_paramName = value;
+				}
+			} else if (m_state == 31) {
 				if (m_paramName == null) {
 					System.err.println("Context parameter value '" + value + "' without name");
 					System.exit(-1);
@@ -73,7 +78,7 @@ public class XmlParser {
 				m_contextParams.put(m_paramName, value);
 				m_paramName = null;
 				m_state = 0;
-			} else if (m_state == 41) { // init-param > param-value
+			} else if (m_state == 41) {
 				if (m_paramName == null) {
 					System.err.println("Servlet parameter value '" + value + "' without name");
 					System.exit(-1);
@@ -85,6 +90,18 @@ public class XmlParser {
 				}
 				p.put(m_paramName, value);
 				m_paramName = null;
+				m_state = 0;
+			} else if (m_state == 50) {
+				if (value.trim().length() > 0) {
+					m_mappingName = value;
+				}
+			} else if (m_state == 51) {
+				if (m_mappingName == null) {
+					System.err.println("Servlet mapping value '" + value + "' without url");
+					System.exit(-1);
+				}
+				m_servletMappings.put(m_mappingName, value);
+				m_mappingName = null;
 				m_state = 0;
 			}
 		}
@@ -103,6 +120,7 @@ public class XmlParser {
 		this.servlets = h.m_servlets;
 		this.contextParams = h.m_contextParams;
 		this.servletParams = h.m_servletParams;
+		this.servletMappings = h.m_servletMappings;
 	}
 	
 	public myServletContext getServletContext() {
